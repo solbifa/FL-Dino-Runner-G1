fdf8ea6a8f7567e76e6eb44c99ca1880e38fd293
solbifa <126725631+solbifa@users.noreply.github.com>
2023-03-05 19:09:10 -0400

    Tarea 1: Implementación del método duck en la clase dinosaur
    Primero agregue el método duck, que define el comportamiento de la acción agacharse.
    El método comienza imprimiendo la cadena "Ducking" en la consola. A continuación, establece el atributo de imagen del objeto en la imagen correspondiente al paso actual de la animación de atenuación. La imagen específica utilizada se determina mediante la indexación en una lista llamada DUCKING mediante la división de enteros para convertir el número de paso actual al índice correcto.
    Luego, el método establece el atributo rect del objeto en un nuevo Rect objeto que define la posición y el tamaño del dinosaurio en la pantalla. La coordenada x del rect se establece en un valor constante POSITION_X, y la coordenada y se establece en un valor que se compensa en 40 píxeles desde el valor constante POSITION_Y. Este desplazamiento coloca el dinosaurio más abajo en la pantalla para simular un movimiento de agachamiento.
    Finalmente, el método incrementa el atributo de paso en 1, para prepararse para el siguiente cuadro de la animación de atenuación.
    Para que la acción de agacharse funcione cada vez que el jugador presione la flecha de Down(Hacia abajo), el método duck se llama en el método update, ya que este método toma user_input como parámetro, que se espera que sea un diccionario de eventos del teclado.
    Luego verifica si la acción actual no es saltar o agacharse. Si no es así, comprueba la entrada del teclado para las teclas de flecha hacia arriba y hacia abajo. Si se presiona la tecla de flecha hacia arriba, establece la acción del dinosaurio para saltar. Si se presiona la tecla de flecha hacia abajo, establece la acción del dinosaurio en agacharse. Si no se presiona ninguna tecla, establece la acción de correr.
    Finalmente, comprueba si el step contador ha llegado a 10 y lo restablece a 0 si es así.
    Nota:
    En la implementación del código duck en la ejecución de este el método jump no funcionaba correctamente, motivo por el que hice unos cambios en el método jump:
    if self.rect.y >= self. POSITION_y: Esta línea comprueba si el objeto ha alcanzado su altura máxima (es decir, si su y coordenada es mayor o igual que su POSITION_y coordenada original).
    self.jump_velocity = self.JUMP_VELOCITY: Esta línea restablece el jump_velocity valor inicial, lo que hará que el objeto comience a caer hacia abajo.
    self.action = DINO_RUNNING: esta línea cambia la acción del objeto a DINO_RUNNING, que es constante o una variable que contiene los datos de la imagen para el estado de ejecución.
    self.rect.y = self.POSITION_y: Esta línea establece la y coordenada del objeto en su POSITION_y coordenada original, lo que lo colocará de nuevo en el suelo.
    Con esta modificación, el dinosaurio seguirá en el aire hasta que su posición Y sea mayor o igual a la posición Y inicial. Cuando esto suceda, la velocidad de salto se restaurará a su valor inicial y la acción del dinosaurio se establecerá en "corriendo" (DINO_RUNNING), y la posición Y del dinosaurio se establecerá en la posición Y inicial para que el dinosaurio aterrice exactamente en ese lugar.

--- dino_runner/components/Dinosaur.py
+++ dino_runner/components/Dinosaur.py
@@ -1,11 +1,11 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import JUMPING, RUNNING
-
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 DINO_RUNNING = "running"
 DINO_JUMPING = "jumping"
+DINO_DUCKING = "ducking"
 
 class Dinosaur(Sprite):
     POSITION_X = 80
@@ -26,10 +26,15 @@ class Dinosaur(Sprite):
         if self.action == DINO_RUNNING:
             self.run()
         elif self.action == DINO_JUMPING:
-           self.jump()
-        if self.action != DINO_JUMPING:
+            self.jump()
+        elif self.action == DINO_DUCKING:
+            self.duck()
+
+        if self.action != DINO_JUMPING or self.action != DINO_DUCKING:
             if user_input[pygame.K_UP]:
                 self.action = DINO_JUMPING
+            elif user_input[pygame.K_DOWN]:
+                self.action = DINO_DUCKING
             else:
                 self.action = DINO_RUNNING
 
@@ -37,22 +42,35 @@ class Dinosaur(Sprite):
             self.step = 0
 
     def jump(self):
-        self.image = JUMPING 
+        self.image = JUMPING
         self.rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
         print("VELOCITY ::", self.jump_velocity)
         print("y ::", self.rect.y)
-        if self.jump_velocity < -self.JUMP_VELOCITY:
+        if self.rect.y >= self.POSITION_y:
             self.jump_velocity = self.JUMP_VELOCITY
             self.action = DINO_RUNNING
             self.rect.y = self.POSITION_y
+#Nota :) Cambios escritos en la descripcion 
+        """if self.jump_velocity < -self.JUMP_VELOCITY:
+            self.jump_velocity = self.JUMP_VELOCITY
+            self.action = DINO_RUNNING
+            self.rect.y = self.POSITION_y """
+
+    def duck(self):
+        print("Ducking")
+        self.image = DUCKING[self.step // 5]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.POSITION_X
+        self.rect.y = self.POSITION_y + 40
+        self.step += 1
 
     def run(self):
-            self.image = RUNNING[self.step // 5] 
-            self.rect = self.image.get_rect()
-            self.rect.x = self.POSITION_X
-            self.rect.y = self.POSITION_y
-            self.step += 1
+        self.image = RUNNING[self.step // 5]
+        self.rect = self.image.get_rect()
+        self.rect.x = self.POSITION_X
+        self.rect.y = self.POSITION_y
+        self.step += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
